f29ae7c32751480a7f8c84324cda5c6034388664
Heart <haert.apisitu@gmail.com>
2024-02-28 15:40:33 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-
+const app = express();
 const XLSX = require('xlsx');
 const path = require('path');
 
-const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get('/population', async (req, res) => {
   try {
