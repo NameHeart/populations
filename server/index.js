d6e1cfb3418732cb5d91d5c95c16bb72397b4030
Heart <haert.apisitu@gmail.com>
2024-02-28 15:37:41 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-// const express = require('express');
-// const cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
-// const XLSX = require('xlsx');
-// const path = require('path');
+const XLSX = require('xlsx');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// app.get('/population', async (req, res) => {
-//   try {
-//     const filePath = path.join(__dirname, 'population-and-demography.csv');
-//     const workbook = XLSX.readFile(filePath);
-//     const sheetName = workbook.SheetNames[0];
-//     const populationData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-//     res.json(populationData);
-//   } catch (error) {
-//     console.error('Error fetching population data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+app.get('/population', async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, 'population-and-demography.csv');
+    const workbook = XLSX.readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const populationData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    res.json(populationData);
+  } catch (error) {
+    console.error('Error fetching population data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-// module.exports = app;
+module.exports = app;
